20df7496b854539b631a3ad795bfd7e952e98c22
rahmanKhanDev <arkhan00555@gamil.com>
2024-09-21 15:44:47 +0530

    set up thymeleaf fragments

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,16 +1,36 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 // page controller
 @Controller
 public class PageController {
-    // home page
+    // home page route
     @GetMapping("/home")
     public String honePage() {
         // retrun lvn
-        System.out.println("home page");
+        System.out.println("home page loading...");
         return "home";
     }
+
+    // about page route
+    @GetMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        // retrun lvn
+        System.out.println("about page loading...");
+        return "about";
+    }
+
+    // services page route
+    @GetMapping("/services")
+    public String servicesPage() {
+        // retrun lvn
+        System.out.println("services page loading...");
+        return "services";
+    }
+
 }
